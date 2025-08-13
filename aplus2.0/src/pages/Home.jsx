c243bb53f87240e061605982824925743688327d
devcowsar <kawsarcse836@gmail.com>
2025-08-13 17:33:21 +0600

    fix: f-commit

--- aplus2.0/src/pages/Home.jsx
+++ aplus2.0/src/pages/Home.jsx
@@ -1,16 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Home = () => {
-
-  
   return (
     <div>
-
       <div className="">
         <h1>Home page</h1>
-        <p className="py-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, consequatur eum. Eius, fugiat delectus doloribus repudiandae laudantium commodi sint quaerat ducimus ad quasi</p>
-        <p className="py-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, consequatur eum. Eius, fugiat delectus doloribus repudiandae laudantium commodi sint quaerat ducimus ad quasi</p>
-        <p className="py-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, consequatur eum. Eius, fugiat delectus doloribus repudiandae laudantium commodi sint quaerat ducimus ad quasi</p>
+        <p className="py-20">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+          consequatur eum. Eius, fugiat delectus doloribus repudiandae
+          laudantium commodi sint quaerat ducimus ad quasi
+        </p>
+        <p className="py-20">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+          consequatur eum. Eius, fugiat delectus doloribus repudiandae
+          laudantium commodi sint quaerat ducimus ad quasi
+        </p>
+        <p className="py-20">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+          consequatur eum. Eius, fugiat delectus doloribus repudiandae
+          laudantium commodi sint quaerat ducimus ad quasi
+        </p>
+        <p className="text-red-50 py-20">Kausar code</p>
       </div>
     </div>
   );
